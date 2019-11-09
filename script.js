f3210339703b60558c5071915696317329119b67
unknown <sloonoopootam@gmail.com>
2019-11-09 07:03:40 +0200

    Work1

---
+++ script.js
@@ -0,0 +1,26 @@
+function perform() {
+    const args = Array.from(perform.arguments);
+    const cb = args.pop();
+    if (typeof cb === "function") {
+        const result = args.map(a => cb(a));
+        this.then = function (cb) {
+            return perform(result, cb);
+        };
+        return this;
+    } else console.error("Last argument is not a callback function");
+}
+
+perform(null, function(value) {
+    console.log(value); // null
+    let param = 1;
+    console.log(param); // 1
+    return param;
+})
+    .then(function(param) {
+        console.log(++param); // 2
+        return param;
+    })
+    .then(function(param) { // param === 2
+        console.log(++param); // 3
+        return param;
+    });
